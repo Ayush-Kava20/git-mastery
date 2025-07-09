a6aca9f6ad75fc6838bf289f60099e8e7c38c5ab
Advant ayush <ayush.kava@advant.xyz>
2025-07-09 15:06:53 +0530

    Get previous code using git stash

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ console.log('Check git reset 2');
 
 //add the new code pervius code is store in stash
 console.log('hello, gujarat');  
+//Add the code which is used when neddeed
+//code
+//code
+//code
+//code
+//code
