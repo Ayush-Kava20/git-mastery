be5c41fda1b8b7ebd90c72697ce3b602c3693e54
Advant ayush <ayush.kava@advant.xyz>
2025-07-09 15:03:27 +0530

    check stash

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 console.log('Hello, World!');
 console.log('Check git reset 1');
 console.log('Check git reset 2');
+
+//add the new code pervius code is store in stash
+console.log('hello, gujarat');  
