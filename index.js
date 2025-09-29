c936069a0b58996bfd9e25821a1201f40fb6852a
Advant ayush <ayush.kava@advant.xyz>
2025-09-29 16:12:04 +0530

    added another line in main

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ console.log('Check git reset 2');
 
 //add the new code pervius code is store in stash
 console.log('Added from main branch');
-console.log("this is ak ")
+console.log('this is ak from main branch');
