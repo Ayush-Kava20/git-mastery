e4d8f1c7239558a71e45d0be1047dcbce43f3170
Advant ayush <ayush.kava@advant.xyz>
2025-09-29 16:54:55 +0530

    add content in feature 1 branch

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ console.log('Check git reset 2');
 
 //add the new code pervius code is store in stash
 console.log('Added from main branch');
-console.log('this is ak from main branch');
-console.log('this is ak from main branch 1');
-console.log('this is ak from main branch 2');
+console.log('this is ak from main');
+console.log('this is ak from main 1');
+console.log('this is ak from main  2');
 console.log('line add from feature 1 branch')
 console.log("this is ak ")
