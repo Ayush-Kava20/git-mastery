7bacb2ea2c85354c1d250deaf0c0a3178cadf1c1
Advant ayush <ayush.kava@advant.xyz>
2025-09-29 16:56:50 +0530

    add one line

--- index.js
+++ index.js
@@ -4,8 +4,5 @@ console.log('Check git reset 2');
 
 //add the new code pervius code is store in stash
 console.log('Added from main branch');
-console.log('this is ak from main branch');
-console.log('this is ak from main branch 1');
-console.log('this is ak from main branch 2');
-console.log('line add from feature 1 branch')
+console.log('Adde only one line from featuer 2 branch')
 console.log("this is ak ")
