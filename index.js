5eb3e372ab33e6c9b48dc92160b4be7261b0dc0c
Advant ayush <ayush.kava@advant.xyz>
2025-09-29 16:09:00 +0530

    add one line

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ console.log('Check git reset 1');
 console.log('Check git reset 2');
 
 //add the new code pervius code is store in stash
-console.log('hello, gujarat');  
-//Add the code which is used when neddeed
-//code
-//code
-//code
-//code
+console.log('line add from feature 1 branch')
 //code
 console.log("this is ak ")
